da0fd1dcebcfc5076fd923b7ae62c5a79f11e04c
kagoz-lab <kagokevin33@gmail.com>
2024-11-27 00:05:38 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = (num1 * num2) ;
+console.log(multiply);
+
+
+const random =  Math.floor(Math.random() * 10) + 1 ;
+//const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+console.log(random);
+
+let num4 = 6;
+let num3 = 10;
+
+const mod = 10 % 6;
+console.log(mod);
+
+let num5 = [10,20,3,4,5,7];
+let max = Math.max(...num5);
+console.log(max);
